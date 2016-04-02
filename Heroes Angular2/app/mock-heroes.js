97800dbfb019daffd0285a8c44533bae4120e169
Isabel Vargas <isabelvargas0@gmail.com>
2016-04-02 12:45:03 -0500

    Se agregan las actividades de los Dojos 1 y 2 correspondientes a Scala y Angular2 respectivamente

---
+++ Heroes Angular2/app/mock-heroes.js
@@ -0,0 +1,25 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Heroes;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("Heroes", Heroes = [
+                { "id": 11, "name": "Batman" },
+                { "id": 12, "name": "IronMan" },
+                { "id": 13, "name": "Spiderman" },
+                { "id": 14, "name": "Thor" },
+                { "id": 15, "name": "SuperMan" },
+                { "id": 16, "name": "Acuaman" },
+                { "id": 17, "name": "Capitán América" },
+            ]);
+        }
+    }
+});
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+//# sourceMappingURL=mock-heroes.js.map
